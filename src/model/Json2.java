8b8b81ac0a573c0a445d3c681eb011f6868fc511
Xcreed <jpm311296@gmail.com>
2015-09-10 03:17:28 -0600

    Reading from different clans

--- src/model/Json2.java
+++ src/model/Json2.java
@@ -10,6 +10,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -24,20 +26,25 @@ import controller.Client;
  * @author Xcreed
  *
  */
-public class Json {
+public class Json2 {
 	
 	
 	private File file=new File("res/stats.json");
-	private String id;
-	private String pw;
-	private long rank;
-	private long importance;
-	private String location;
-	private boolean creator;
-	private Clan clan;
-	private String relicLocation;
-	private String clanName;
-	private DoubleLinkedList clansList = new DoubleLinkedList();
+	public String id;
+	public String pw;
+	public long rank;
+	public long importance;
+	public String location;
+	public boolean creator;
+	public Clan clan;
+	public String relicLocation;
+	public String clanName;
+	public DoubleLinkedList<?> clansList = new DoubleLinkedList<Object>();
+	public DoubleLinkedList<?> weapons = new DoubleLinkedList<Object>();
+	public DoubleLinkedList<?> defenses = new DoubleLinkedList<Object>();
+	public int iron;
+	public int wood;
+	public int powder;
 	private JSONParser parser = new JSONParser();
 	
 	/**
@@ -47,7 +54,7 @@ public class Json {
 	 * @param rank 		Rank the user is at the moment
 	 * @param importance	The decision's importance while doing something
 	 */
-	public Json(String id, String pw, int rank, int importance) {
+	public Json2(String id, String pw, int rank, int importance) {
 		this.pw = pw;
 		this.id = id;
 		this.rank = rank;
@@ -58,7 +65,7 @@ public class Json {
 	/**
 	 * To instanciate the class with no parameters
 	 */
-	public Json() {
+	public Json2() {
 		// TODO Auto-generated constructor stub
 	}
 
@@ -161,17 +168,99 @@ public class Json {
 	   JSONObject jsonObject = (JSONObject) obj;
 	   
 	   //Each clan in the file <This is an array>
-	   JSONArray clan = (JSONArray) jsonObject.get("clans");
-	   /**iteratorClan.next() = relics**/
+	   JSONArray clans = (JSONArray) jsonObject.get("clans");
+	   System.out.println("Complete file " + clans); 
+
 	   
-	   //List of clients of the clan
-	   JSONObject clients = (JSONObject) clan.get(0);
-	   JSONArray clientsArray = (JSONArray) clients.get("clients");
-	   //List of relics of the clan
-	   JSONObject relics = (JSONObject) clan.get(1);
-	   JSONArray relicsArray = (JSONArray) relics.get("relics");
+	   //Takes each clan separately
+	   for (int i = 0; i < clans.size(); i++) {
+		   
+		   JSONObject clanObj = (JSONObject) clans.get(i);
+		   //System.out.println("ClanObj " + clanObj );
+		   
+		   //Grabs the name of the Clan
+		   String ClanName = (String) clanObj.get("clan name");
+		   //System.out.println("ClanName " + ClanName);
+		   
+		   //Gets all the stats of the clan
+		   JSONArray clanStatsArray = (JSONArray) clanObj.get("stats"); 
+		   //System.out.println("ClanStatsArray " + clanStatsArray);
+
+		   //List of clients of the clan
+		   JSONObject clients = (JSONObject) clanStatsArray.get(0);
+		   JSONArray clientsArray = (JSONArray) clients.get("clients"); 
+		   //System.out.println("ClientsArray " + clientsArray); //Add the code commented below
+		   
+		 //Gets the stats from the clients 
+		   for (int j = 0; j < clientsArray.size(); j++) {
+			   
+			   JSONObject client = (JSONObject) clientsArray.get(j);
+			   //ID of the client
+			   id = (String) client.get("id");
+			   System.out.println("Username: "+ id);
+			   //Rank of the client
+			   rank = (long) client.get("rank");
+			   System.out.println("Rank: " + rank);
+			   //Importance of the user
+			   importance = (long) client.get("importance");
+			   System.out.println("Importance: " + importance);
+			   //Location of each user
+			   location = (String) client.get("location");
+			   System.out.println("Location: " + location);
+			   //If the user is creator or not
+			   creator = (boolean) client.get("creator");
+			   System.out.println("Creator: " + creator);
+			   //Which clan is the user from
+			   String member = (String) client.get("clan");
+			   System.out.println("Is member from the clan: " + member);
+			   
+			   //Creates a new client when the file is read
+			   //Client gameMember = new Client(id, pw, rank, importance, location, member);
+		   }
+		   
+		   //List of relics of the clan
+		   JSONObject relics = (JSONObject) clanStatsArray.get(1);
+		   JSONArray relicsArray = (JSONArray) relics.get("relics");
+		   //System.out.println("RelicsArray " + relicsArray); //Add the code commented below
+		   
+		   for (int j = 0; j < relicsArray.size(); j++) {
+			   
+			   JSONObject relic = (JSONObject) relicsArray.get(j);
+			   //Location of the relic
+			   relicLocation = (String) relic.get("location");
+			   System.out.println("Relic is located at: " + relicLocation);			   
+			   
+		   }
+		   
+		   JSONObject weapons = (JSONObject) clanStatsArray.get(2);
+		   JSONObject weaponsList = (JSONObject) weapons.get("weapons"); //Only recognizes one "weapon"
+		   System.out.println("Weapons: " + weaponsList);
+		   
+		   //Gets all the resources *****WORKING*******
+		   JSONObject resourcesObj = (JSONObject) clanStatsArray.get(3);
+		   JSONArray resourcesArray = (JSONArray) resourcesObj.get("resources");
+		   JSONObject resources = (JSONObject) resourcesArray.get(0);
+		   System.out.println(resources);
+		   System.out.println("iron" + resources.get("iron"));
+		   System.out.println("wood" + resources.get("wood"));
+		   System.out.println("powder" + resources.get("powder"));
+		   
+		   JSONObject defensesObj = (JSONObject) clanStatsArray.get(4);
+		   JSONObject defensesArray = (JSONObject) defensesObj.get("defenses");
+		   JSONObject defenses = (JSONObject) resourcesArray.get(0);
+		   System.out.println(defenses);
+
+	   }
 	   
 	   
+	   
+	   /*
+	   for (Object clanObj : clans) {
+		   System.out.println(clanObj);
+	   }
+	   */
+	   
+	   /*
 	   //Gets the stats from the clients 
 	   for (int i = 0; i < clientsArray.size(); i++) {
 		   
@@ -209,6 +298,7 @@ public class Json {
 		   
 		   
 	   }
+	   */
 
 	   System.out.println("Done reading file");
 
---
+++ src/model/main.java
@@ -0,0 +1,11 @@
+package model;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Json2 json = new Json2();
+		json.read();
+	}
+
+}
