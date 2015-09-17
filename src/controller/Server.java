e1d25b478e5e0baa0436b4c13bf8274bb87f28fa
morazan <morazan@monge>
2015-09-16 19:19:28 -0600

    server

--- src/controller/Server.java
+++ src/controller/Server.java
@@ -1,30 +1,43 @@
 package controller;
 
-public class Server implements Observer{
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-	//private Image Map = new Image();
-	//private List clansCreated = new ArrayList<Clans>();
-	
-	public Server() {
+public class Server implements Observer{
+	public static void main(String[] args){
+		Thread t = new Thread(){
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				System.out.println("Server is running and listening");
+				try {
+					ServerSocket ss = new ServerSocket(7777);
+					while(true){
+						Socket s= ss.accept();
+						DataInputStream dis = new DataInputStream(s.getInputStream());
+						System.out.println("Received from client: "+dis.readUTF());
+						dis.close();
+						s.close();
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+			
+		};
+		t.start();
+		
 		
 	}
-	
+
+	@Override
 	public void update() {
-		
-	}
-	
-	//Creates and places a gun in the terrain
-	//public Gun createGun() {
-		
-	//}
-	
-	//Creates a defense randomly in the terrain
-	//public Defense createDefense() {
-		
-	//}
-	
-	//Creates the terrain where it will be played
-	public void createTerrain() {
+		// TODO Auto-generated method stub
 		
 	}
 }
